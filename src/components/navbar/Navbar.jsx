38d13db788ab5a0c7fe940435d1c0c23752c1bb8
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-01-11 22:39:16 -0300

    header and mobile menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import logo from '../../assets/logo.svg'
 
 
+const Menu = () => (
+  <>
+    <p> <a href='#home'>Início</a> </p>
+    <p> <a href='#wgpt3'>O que é  o GPT3?</a> </p>
+    <p> <a href='#possibility'>Open AI</a> </p>
+    <p> <a href='#f&atures'>Cases de estudo</a> </p>
+    <p> <a href='#blog'>Blog</a> </p>
+  </>
+)
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
@@ -12,17 +24,32 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className='gpt3__navbar-links_container'>
-          <p> <a href='#home'>Início</a> </p>
-          <p> <a href='#wgpt3'>O que é  o GPT3?</a> </p>
-          <p> <a href='#possibility'>Open AI</a> </p>
-          <p> <a href='#f&atures'>Cases de estudo</a> </p>
-          <p> <a href='#blog'>Blog</a> </p>
+          <Menu />
         </div>
       </div>
 
       <div className='gpt3__navbar-sign'>
         <p>Entrar</p>
         <button type='button'>Inscrever-se</button>
+      </div>
+
+      <div className='gpt3__navbar-menu'>
+        {toggleMenu
+          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+        }
+
+        {toggleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'>
+              <Menu />
+              <div className='gpt3__navbar-menu_container-links-sign'>
+                <p>Entrar</p>
+                <button type='button'>Inscrever-se</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './header.css'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Vamos juntos construir algo incrível com a GPT-3 OpenAI</h1>
+        <p>Eu nulla eiusmod duis adipisicing commodo anim tempor elit qui ex eiusmod laborum exercitation. Commodo sunt mollit commodo ex consequat reprehenderit irure.</p>
+        <div className='gpt3__header-content__input'>
+          <input type="email" placeholder='Seu Endereço De E-mail' />
+          <button type='button'>Começar</button>
+        </div>
+
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt="people"/>
+          <p>1600 pessoas solicitaram acesso nas últimas 24 horas</p>
+        </div>
+      </div>
+
+      <div className='gpt3__header-image'>
+          <img src={ai} alt="ai"/>
+        </div>
+        
+    </div>
   )
 }
 
